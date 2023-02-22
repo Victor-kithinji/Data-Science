47f11f3952040aa78bac8cb03dd314f7e22a01ec
Victor-kithinji <victorkithinji@gmail.com>
2023-02-22 16:05:11 +0300

    navigating the tree

--- navigating/tree.py
+++ navigating/tree.py
@@ -12,12 +12,30 @@ and they lived at the bottom of a well.</p>
 <p class="story">...</p>
 """
 
-
+from bs4 import Comment
 from bs4 import BeautifulSoup
 soup = BeautifulSoup(html_doc, 'html.parser')
 for string in soup.strings:
     print(repr(string))
+    #prints the strings
 for tag in soup.find_all([True]):
     print(tag.name)
+    #prints the tags in the page
 for string in soup.stripped_strings:
     print(repr(string))
+    #removes the whitespaces between strings
+new_comment = Comment("Nice to see you.")
+tag.append(new_comment)
+tag
+# <b>Hello there<!--Nice to see you.--></b>
+tag.contents
+markup = "<p>I want <b>pizza</b> and more <b>pizza</b>!</p>"
+soup = BeautifulSoup(markup, 'html.parser')
+first_b, second_b = soup.find_all('b')
+print(first_b == second_b)
+# True
+
+print(first_b.previous_element == second_b.previous_element)
+import copy
+p_copy = copy.copy(soup.p)
+print(p_copy)
